32934cbefc43ca22bae92938858029087ed98e1a
carlosFGM <carlosfgm04@gmail.com>
2023-11-29 14:36:40 -0600

    actualizacion del MemoramaActivity

--- app/src/main/java/com/example/geoexplora/MemoramaActivity.java
+++ app/src/main/java/com/example/geoexplora/MemoramaActivity.java
@@ -11,8 +11,5 @@ public class MemoramaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memorama);
-
-        TextView textView = findViewById(R.id.textViewMensaje);
-        textView.setText("Hola Mundo - Memorama");
     }
 }
